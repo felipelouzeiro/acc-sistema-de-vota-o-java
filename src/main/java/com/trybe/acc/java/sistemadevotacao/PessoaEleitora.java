80ec051a5169eccc53dbdb03a1fb2344676c35bf
felipe <fl.louzeiro@gmail.com>
2022-03-30 18:04:17 -0300

    feat: implementa classe pessoaEleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,31 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa {
+
+  private String cpf;
+  // nome --> herdado da classe pessoa;
+
+  public PessoaEleitora(String cpf, String nome) {
+    this.cpf = cpf;
+    this.nome = nome;
+  }
+
+  @Override
+  public String getNome() {
+    return nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 
 }

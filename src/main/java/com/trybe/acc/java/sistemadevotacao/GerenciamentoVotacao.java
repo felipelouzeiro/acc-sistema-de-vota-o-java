74d0b1b5c8c36c96219f8f6fd35a4c18e48e91e6
felipe <fl.louzeiro@gmail.com>
2022-03-30 18:48:05 -0300

    feat: implementa metodo cadastrarPessoaCandidata em gerenciamentoVotacao

--- src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/GerenciamentoVotacao.java
@@ -11,13 +11,14 @@ public class GerenciamentoVotacao {
    * Método cadastrarPessoaCandidata.
    */
   public void cadastrarPessoaCandidata(String nome, int numero) {
-    // verifica numero
-    // se numero da pessoa candidata já registrado:
-    System.out.println("Número pessoa candidata já utilizado!");
-    // se não:
-    PessoaCandidata pessoaCandidata = new PessoaCandidata();
-    // passando o nome e numero para o construtor dessa classe;
-    // e adciona no array pessoasCandidatas;
+    for (PessoaCandidata candidato : pessoasCandidatas) {
+      if (candidato.getNumero() == numero) {
+        System.out.println("Número pessoa candidata já utilizado!");
+      } else {
+        PessoaCandidata pessoaCandidata = new PessoaCandidata(numero, nome);
+        pessoasCandidatas.add(pessoaCandidata);
+      }
+    }
   }
 
   /**

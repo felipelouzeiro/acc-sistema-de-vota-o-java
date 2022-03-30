4bb0fc12b9fd971e8d2df71a0a8a39e6ed7184b6
felipe <fl.louzeiro@gmail.com>
2022-03-30 16:08:21 -0300

    feat: cria primeiro menu-cadastro de pessoas candidatas

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -1,10 +1,34 @@
 package com.trybe.acc.java.sistemadevotacao;
 
+import java.util.Scanner;
+
 public class Principal {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
+    GerenciamentoVotacao gVotacao = new GerenciamentoVotacao();
+    Scanner scanner = new Scanner(System.in);
+
+    short opcao;
+
+    do {
+      System.out.println("Cadastrar pessoa candidata?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.println("Entre com o número correspondente à opção desejada:");
+
+      opcao = scanner.nextShort();
+
+      if (opcao == 1) {
+        System.out.println("Entre com o nome da pessoa candidata:");
+        String nomePessoaCandidata = scanner.next();
+        System.out.println("Entre com o número da pessoa candidata:");
+        Short numeroPessoaCandidata = scanner.nextShort();
+
+      } else if (opcao != 2) {
+        System.out.println("Entre com uma opção válida!");
+      }
+    } while (opcao != 2);
   }
 
 }

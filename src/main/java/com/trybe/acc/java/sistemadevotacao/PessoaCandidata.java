fcbcc1c99901ad4bc0781d0c2a778e98093c4a69
felipe <fl.louzeiro@gmail.com>
2022-03-30 18:06:21 -0300

    feat: implementa classe PessoaCandidata

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,45 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+
+  // nome -> herdado da classe Pessoa.
+  private int numero;
+  private int votos;
+
+  public PessoaCandidata(int numero, String nome) {
+    this.numero = numero;
+    this.nome = nome;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  @Override
+  public String getNome() {
+    return nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public void recebeVoto() {
+    this.votos += 1;
+  }
 
 }

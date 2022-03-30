12bd70fd6e429918ac3cdb8683f18a29a79f5f94
felipe <fl.louzeiro@gmail.com>
2022-03-30 18:05:51 -0300

    feat: implementa classe Pessoa

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -6,11 +6,11 @@ public class Principal {
 
   public static void main(String[] args) {
 
-    GerenciamentoVotacao gVotacao = new GerenciamentoVotacao();
+    GerenciamentoVotacao gerenciamentoVotacao = new GerenciamentoVotacao();
     Scanner scanner = new Scanner(System.in);
 
     short opcao;
-
+    // menu cadastro de candidatos;
     do {
       System.out.println("Cadastrar pessoa candidata?");
       System.out.println("1 - Sim");
@@ -29,6 +29,50 @@ public class Principal {
         System.out.println("Entre com uma opção válida!");
       }
     } while (opcao != 2);
+
+    // menu cadastro de eleitores;
+    do {
+      System.out.println("Cadastrar pessoa eleitora?");
+      System.out.println("1 - Sim");
+      System.out.println("2 - Não");
+      System.out.println("Entre com o número correspondente à opção desejada:");
+
+      opcao = scanner.nextShort();
+
+      if (opcao == 1) {
+        System.out.println("Entre com o nome da pessoa eleitora:");
+        String nomePessoaEleitora = scanner.next();
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpfPessoaEleitora = scanner.next();
+
+      } else if (opcao != 2) {
+        System.out.println("Entre com uma opção válida!");
+      }
+    } while (opcao != 2);
+
+    // menu de votação
+    do {
+      System.out.println("Entre com o número correspondente à opção desejada:");
+      System.out.println("1 - Votar");
+      System.out.println("2 - Resultado Parcial");
+      System.out.println("3 - Finalizar Votação");
+
+      opcao = scanner.nextShort();
+
+      if (opcao == 1) {
+        System.out.println("Entre com o cpf da pessoa eleitora:");
+        String cpfPessoaEleitoraVotacao = scanner.next();
+        System.out.println("Entre com o numero da pessoa candidata:");
+        Short numeroPessoaCandidataVotacao = scanner.nextShort();
+
+      } else if (opcao == 2) {
+        System.out.println("Nome: Maria - 1 votos ( 100.0% ) \n Total de votos: 1");
+      } else if (opcao != 3) {
+        System.out.println("Entre com uma opção válida!");
+      }
+    } while (opcao != 3);
+
+    System.out.println("Nome: Maria - 1 votos ( 100.0% ) \n Total de votos: 1");
   }
 
 }
